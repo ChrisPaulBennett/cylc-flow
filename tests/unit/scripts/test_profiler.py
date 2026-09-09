609d4d888403ed702624f16327dda56baafbd178
christopher.bennett <christopher.bennett@metoffice.gov.uk>
2026-09-09 14:24:11 +0100

    Changed max rss values to mega bytes to decrease number size

--- tests/unit/scripts/test_profiler.py
+++ tests/unit/scripts/test_profiler.py
@@ -102,9 +102,11 @@ def test_get_resource_usage():
 def test_parse_memory_file(tmpdir):
     """It should return the memory usage of the process."""
     mem_file_v1 = tmpdir.join("memory_file_v1.txt")
-    mem_file_v1.write('total_rss=1024')
+    # 2 MiB in bytes
+    mem_file_v1.write('total_rss=2097152')
     mem_file_v2 = tmpdir.join("memory_file_v2.txt")
-    mem_file_v2.write('anon=666')
+    # 3 MiB in bytes
+    mem_file_v2.write('anon=3145728')
     cpu_file = tmpdir.join("cpu_file.txt")
     cpu_file.write('5678')
     mem_allocated_file = tmpdir.join("memory_allocated.txt")
@@ -133,9 +135,9 @@ def test_parse_memory_file(tmpdir):
         parse_memory_file(bad_process_object)
     assert "Unable to find memory usage data" in str(excinfo.value)
 
-    # Test the parse_memory_file function
-    assert parse_memory_file(good_process_object_v1) == 1024
-    assert parse_memory_file(good_process_object_v2) == 666
+    # Test the parse_memory_file function (values reported in megabytes)
+    assert parse_memory_file(good_process_object_v1) == 2
+    assert parse_memory_file(good_process_object_v2) == 3
 
 
 def test_parse_cpu_file(tmpdir):
@@ -217,7 +219,8 @@ def test_parse_memory_allocated(tmp_path_factory):
     """It should return the memory allocated to the process."""
     good_mem_dir = tmp_path_factory.mktemp("mem_dir")
     mem_allocated_file = good_mem_dir / "memory.max"
-    mem_allocated_file.write_text('99999')
+    # 100 MiB in bytes
+    mem_allocated_file.write_text('104857600')
 
     # We currently do not track memory allocated for cgroups v1
     good_process_object_v1 = Process(
@@ -242,7 +245,7 @@ def test_parse_memory_allocated(tmp_path_factory):
         max_rss=0)
 
     assert parse_memory_allocated(good_process_object_v1) == 0
-    assert parse_memory_allocated(good_process_object_v2) == 99999
+    assert parse_memory_allocated(good_process_object_v2) == 100
     with pytest.raises(CylcProfilerError) as excinfo:
         parse_memory_file(bad_process_object_v2_1)
     assert "Unable to find memory usage data" in str(excinfo.value)
@@ -287,8 +290,8 @@ def test_parse_memory_allocated(tmp_path_factory):
 
     # Add a memory.max file with a value to the top level directory
     # and check it is read
-    mem_file_1.write_text("99999")
-    assert parse_memory_allocated(bad_process_object_v2_2) == 99999
+    mem_file_1.write_text("104857600")
+    assert parse_memory_allocated(bad_process_object_v2_2) == 100
 
 
 def test_get_cgroup_name_file_not_found(mocker):
